bd5fb670cf34ede6f4aee701502839e1abb72fb0
Tiger Rahman <tiger.rahman86@gmail.com>
2016-07-28 22:38:13 -0400

    completed basic twentyone

--- Lesson_4/twentyone.rb
+++ Lesson_4/twentyone.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # twentyone.rb
 # 1. Initialize deck
 # 2. Deal cards to player and dealer
@@ -9,10 +11,11 @@
 # 6. If dealer bust, player wins.
 # 7. Compare cards and declare winner.
 
-SUITS = ['Hearts','Clubs', 'Diamonds', 'Spades'].freeze
-CARD_VALUES = {'2' => 2,  '3' => 3, '4' => 4, '5' => 5, 
-              '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'Jack' => 10, 
-              'Queen' => 10, 'King' => 10, 'Ace' => 10 }.freeze
+SUITS = ['Hearts', 'Clubs', 'Diamonds', 'Spades'].freeze
+CARD_VALUES = { '2' => 2, '3' => 3, '4' => 4, '5' => 5,
+                '6' => 6, '7' => 7, '8' => 8, '9' => 9,
+                '10' => 10, 'Jack' => 10, 'Queen' => 10,
+                'King' => 10, 'Ace' => 10 }.freeze
 CARD_TYPES = CARD_VALUES.keys.freeze
 
 def prompt(msg)
@@ -21,9 +24,11 @@ end
 
 def make_cards
   cards = []
-    CARD_TYPES.each do |type|
-      SUITS.each { |name| cards << [type, name, CARD_VALUES.values_at(type)].flatten }
-      end 
+  CARD_TYPES.each do |type|
+    SUITS.each do |name|
+      cards << [type, name, CARD_VALUES.values_at(type)].flatten
+    end
+  end
   cards
 end
 
@@ -40,13 +45,13 @@ def player_draws_card(draw_pile, player_hand)
   player_hand << drawn_card
   available_cards(draw_pile, drawn_card)
   player_hand
+  # binding.pry
 end
 
-
 def dealer_draws_card(draw_pile, dealer_hand)
-#   give the dealer a random available card
+  #   give the dealer a random available card
   drawn_card = draw_pile.sample
-#   make an array of what cards the dealer has
+  #   make an array of what cards the dealer has
   dealer_hand << drawn_card
   available_cards(draw_pile, drawn_card)
   dealer_hand
@@ -60,7 +65,14 @@ end
 # tell what the value of a player's hand is at a given point in time
 def value_of_hand(player_hand)
   player_total = 0
-  player_hand.each { |type, name, value| player_total += value }
+  player_hand.each do |type, _name, value|
+    if type == 'Ace' && player_total < 12
+      value = 10
+    elsif type == 'Ace' && player_total > 11
+      value = 1
+    end
+    player_total += value
+  end
   player_total
 end
 
@@ -74,6 +86,20 @@ def busted?(total)
   end
 end
 
+def assign_winner(player_total, dealer_total)
+  if player_total > 22
+    prompt "You busted. Dealer wins!"
+  elsif dealer_total == 0
+    prompt "Dealer's turn..."
+  elsif player_total == dealer_total
+    prompt "You managed a tie."
+  elsif (21 - player_total) < (21 - dealer_total)
+    prompt "Congratulations! Player wins."
+  else
+    prompt "Tough. Dealer wins."
+  end
+end
+
 player_hand = []
 dealer_hand = []
 player_move = ''
@@ -82,44 +108,44 @@ draw_pile = initial_deck
 player_total = 0
 dealer_total = 0
 
-# the dealer draws and hands are compared
+# this loop is the player's set of turns to get to 21
 loop do
-
-  # this loop is the player's set of turns to get to 21
+  # give player an opportunity to hit or stay
   loop do
-
-    # give player an opportunity to hit or stay
-    loop do
-      prompt "Press 'H' to hit and draw a card or 'S' to stay."
-      player_move = gets.chomp.downcase
-      break if player_move == 'h' || player_move == 's' 
-      prompt "I didn't understand, please try again."
-    end  
-
-    if player_move == 'h'
-      player_draws_card(draw_pile, player_hand)
-      prompt "You have the following cards in your hand:"
-      human_friendly_names(player_hand)
-    elsif player_move == 's'
-        prompt "You chose to stay."
-        player_total = value_of_hand(player_hand)
-    end
-    prompt "The value of your hand is now #{value_of_hand(player_hand)}"
-    break if player_move == 's'
+    prompt "Press 'H' to hit and draw a card or 'S' to stay."
+    player_move = gets.chomp.downcase
+    break if player_move == 'h' || player_move == 's'
+    prompt "I didn't understand, please try again."
   end
 
-  prompt "There are #{draw_pile.count} cards remaining."
-  # check to see if the player busts - if so, dealer wins automatically
-  busted?(player_total)
-
-  # dealer's turns
-  while dealer_total < 18
-    dealer_draws_card(draw_pile, dealer_hand)
-    dealer_total
+  if player_move == 'h'
+    player_draws_card(draw_pile, player_hand)
+    prompt "You have the following cards in your hand:"
+    human_friendly_names(player_hand)
+  elsif player_move == 's'
+    prompt "You chose to stay."
+    player_total = value_of_hand(player_hand)
   end
-  prompt "Dealer has the following cards in their hand:"
-  human_friendly_names(dealer_hand)
-  dealer_total = value_of_hand(dealer_hand)
-  busted?(dealer_total)
-
+  prompt "The value of your hand is now #{value_of_hand(player_hand)}"
+  # busted?(player_total)
+  break if player_move == 's'
 end
+
+prompt "There are #{draw_pile.count} cards remaining."
+# check to see if the player busts - if so, dealer wins automatically
+
+# dealer's turns
+
+loop do
+  if dealer_total <= 17
+    dealer_draws_card(draw_pile, dealer_hand)
+    prompt "Dealer has the following cards in their hand:"
+    human_friendly_names(dealer_hand)
+    dealer_total = value_of_hand(dealer_hand)
+  end
+  prompt "The value of the dealer's hand is now #{value_of_hand(dealer_hand)}"
+  break if dealer_total > 17
+end
+
+# the dealer draws and hands are compared
+assign_winner(player_total, dealer_total)
